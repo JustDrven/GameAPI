2833d5101be8c12817b46ca795dd36a0b549dee0
JustDrven <petr.nemec11111@gmail.com>
2023-04-24 18:03:08 +0200

    Create config in method onEnabled()

--- src/main/java/cz/drven/gameapi/Main.java
+++ src/main/java/cz/drven/gameapi/Main.java
@@ -1,24 +1,35 @@
 package cz.drven.gameapi;
 
 import cz.drven.gameapi.listeners.PlayerListener;
+import cz.drven.gameapi.listeners.ServerListener;
 import cz.drven.gameapi.object.Colors;
 import cz.drven.gameapi.object.Logger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class Main extends JavaPlugin {
 
-    public static String PREFIX = Colors.format("&8[&cGame&8] &r");
+    public static final String PREFIX = Colors.format("&8[&cServer&8] &r");
 
     @Override
     public void onEnable() {
+        try {
+            saveDefaultConfig();
 
+            Logger.log(Logger.LogType.INFO, "&aConfig was been created!");
+
+        } catch (Exception ex) {
+            Logger.log(Logger.LogType.WARNING, "&cConfig was been deleted!");
+        }
         try {
             // setup
             PluginManager pm = getServer().getPluginManager();
 
             // listeners
             pm.registerEvents(new PlayerListener(), this);
+            pm.registerEvents(new ServerListener(this), this);
 
             Logger.log(Logger.LogType.INFO, "&aAPI was been enabled!");
         } catch (Exception ex) {
