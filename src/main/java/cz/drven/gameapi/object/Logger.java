bf3a98737bea65433d39cf46d989831120ff26a5
JustDrven <petr.nemec11111@gmail.com>
2023-04-24 17:17:21 +0200

    Logger

---
+++ src/main/java/cz/drven/gameapi/object/Logger.java
@@ -0,0 +1,15 @@
+package cz.drven.gameapi.object;
+
+import org.bukkit.Bukkit;
+
+public class Logger {
+
+    public static void log(LogType type, String s) {
+        Bukkit.getServer().getConsoleSender().sendMessage(Colors.format("&8[&cGameAPI&8] &7(&e" + type.name() + "&7) &8- &r" + s));
+    }
+
+    public enum LogType {
+        INFO, ERROR, WARNING
+    }
+
+}
