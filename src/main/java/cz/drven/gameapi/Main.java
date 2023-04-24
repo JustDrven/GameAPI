3694c3505c0410cb6c602dd53474b44c924f1925
JustDrven <petr.nemec11111@gmail.com>
2023-04-24 17:44:46 +0200

    Register listener

--- src/main/java/cz/drven/gameapi/Main.java
+++ src/main/java/cz/drven/gameapi/Main.java
@@ -1,7 +1,9 @@
 package cz.drven.gameapi;
 
+import cz.drven.gameapi.listeners.PlayerListener;
 import cz.drven.gameapi.object.Colors;
 import cz.drven.gameapi.object.Logger;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -10,10 +12,18 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         try {
+            // setup
+            PluginManager pm = getServer().getPluginManager();
+
+            // listeners
+            pm.registerEvents(new PlayerListener(), this);
+
             Logger.log(Logger.LogType.INFO, "&aAPI was been enabled!");
         } catch (Exception ex) {
             Logger.log(Logger.LogType.ERROR, "Error with API");
         }
+
     }
 }
