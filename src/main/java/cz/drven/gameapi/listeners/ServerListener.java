3a747aab043aaa7595d76a9a1d8872d2b406a943
JustDrven <petr.nemec11111@gmail.com>
2023-04-24 17:56:09 +0200

    Server motd

---
+++ src/main/java/cz/drven/gameapi/listeners/ServerListener.java
@@ -0,0 +1,31 @@
+package cz.drven.gameapi.listeners;
+
+import cz.drven.gameapi.Main;
+import cz.drven.gameapi.game.GameState;
+import cz.drven.gameapi.object.Colors;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+public class ServerListener implements Listener {
+
+    private Main pl;
+
+    public ServerListener(Main pl) {
+        this.pl = pl;
+    }
+
+    @EventHandler
+    public void onPing(ServerListPingEvent e) {
+        if (GameState.status == GameState.WAITING) {
+            e.setMotd(Colors.format(pl.getConfig().getString("game.status.waiting")));
+        }
+        if (GameState.status == GameState.INGAME) {
+            e.setMotd(Colors.format(pl.getConfig().getString("game.status.ingame")));
+        }
+        if (GameState.status == GameState.RESTARTING) {
+            e.setMotd(Colors.format(pl.getConfig().getString("game.status.restarting")));
+        }
+    }
+
+}
